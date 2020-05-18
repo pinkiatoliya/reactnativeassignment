e2f7e324fa7c47500c305366b9f793e30f86bc9d
Pinki Dhakad <pinki.dhakad@techment.com>
2020-05-18 18:09:24 +0530

    Asteroid search feature added

---
+++ screens/DetailScreen.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import {Card, CardItem, Body, Spinner, H3 } from 'native-base';
+
+const API_KEY = '5kMDuVV8bMJOK85DvE83hdLYCBPgV3hCOIei45HL';
+
+class DetailScreen extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            asteroidData: {},
+            isDataFound: false,
+            isLoading: true
+        }
+    }
+
+    componentDidMount = () => {
+        this.setState({ isLoading: true });
+        fetch('https://api.nasa.gov/neo/rest/v1/neo/' + this.props.route.params + '?api_key=' + API_KEY).then(res => res.json()).then(res => {
+            console.log(res);
+            this.setState({
+                ...this.state,
+                asteroidData: res,
+                isDataFound: true,
+                isLoading: false
+            })
+        }).catch(error => {
+            console.log(error);
+            this.setState({
+                asteroidData: {},
+                isDataFound: false,
+                isLoading: false
+            })
+        })
+    }
+    render() {
+        const localAsteroidData = this.state.asteroidData;
+        return (
+            <View style={styles.screen}>
+                 {this.state.isLoading?  <Spinner color='blue' /> : null}
+                 {(this.state.isDataFound && !this.state.isLoading) ? 
+                 <Card style={styles.card}>
+                 <CardItem header bordered>
+                     <H3>Asteroid Details</H3>
+                 </CardItem>
+                 <CardItem bordered>
+                     <Body>
+                         <Text>Name: &nbsp; {localAsteroidData.name} </Text>
+                         <Text>nasa_jpl_url:&nbsp; {localAsteroidData.nasa_jpl_url} </Text>
+                         <Text>is_potentially_hazardous_asteroid:&nbsp; {localAsteroidData.is_potentially_hazardous_asteroid ? 'True': 'False' } </Text>
+                     </Body>
+                 </CardItem>
+             </Card>
+         : <Text>No Data Found</Text>}
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        alignItems: "center",
+        justifyContent: "center"
+    },
+    card: {
+        width: '90%'
+    }
+})
+export default DetailScreen;
---
+++ screens/HomeScreen.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { View, Card, Button, Text, Form, Item, Label, Input } from 'native-base';
+
+const API_KEY = '5kMDuVV8bMJOK85DvE83hdLYCBPgV3hCOIei45HL';
+class HomeScreen extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            asteroidID: null,
+
+        }
+    }
+
+    getRandomIndex = (max)=>{
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    getRandomAsteroid = ()=>{
+        console.log('random function');
+      fetch('https://api.nasa.gov/neo/rest/v1/neo/browse?api_key='+ API_KEY).then(response=> response.json()).then(response=>{
+              let dataArray = response.near_earth_objects;
+              const randomIndex = this.getRandomIndex(response.page.size);
+              console.log(randomIndex, response.page.size);
+              const asteroidID = dataArray[randomIndex].id;
+              this.commonPageCall(asteroidID);
+      }).catch(error=>{
+
+      })
+    };
+
+    navigateToDetailPage = ()=>{
+      this.commonPageCall(this.state.asteroidID);
+    }
+
+    commonPageCall = (asteroidID)=>{
+        this.props.navigation.navigate('Detail', asteroidID);
+    }
+
+    textChangeHandler = (e)=>{
+        if(!!e){
+            this.setState({
+                asteroidID: e.replace(/[^0-9]/g, '')
+            });
+        }else{
+            this.setState({
+                asteroidID: null
+            });
+        }
+    }
+    render() {
+        const asteroidID = this.state.asteroidID;
+        return (
+            <Card style={{padding: 15}}>
+                <Form>
+                    <Item floatingLabel>
+                        <Label>Enter Asteroid ID</Label>
+                        <Input value={asteroidID}
+                        onChangeText = {this.textChangeHandler}
+                        maxLength={7}/>
+                    </Item>
+                    </Form>
+                    <View style={{flexDirection: "row", justifyContent: 'flex-end', marginVertical: 10}}>
+                        <Button 
+                        primary 
+                        style={{marginHorizontal: 5, padding: 10}}onPress={this.navigateToDetailPage} 
+                        disabled = {!!!asteroidID}
+                        ><Text>Submit</Text></Button>
+
+                        <Button success style={{marginHorizontal: 5, padding: 10}}onPress={this.getRandomAsteroid}><Text>Random Asteroid</Text></Button>
+                    </View>
+               
+            </Card>
+        );
+    }
+}
+
+export default HomeScreen;
